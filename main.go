7a3eabd98feb181d956db529d960ae3a523b760a
Jason Senecharles <jasons11@bu.edu>
2026-06-21 11:49:10 -0400

    Adding hardcoded middleware for api keys

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
+
+// authenticating api key
+func apiKeyAuth() gin.HandlerFunc {
+	// valid keys
+	validKeys := map[string]bool{
+		"abc123":        true,
+		"my-secret-key": true,
+	}
+
+	// checking if they key is valid
+	return func(c *gin.Context) {
+		apiKey := c.GetHeader("x-api-key")
+
+		if !validKeys[apiKey] {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorized: invalid or missing API key",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 	// target backend service
@@ -17,9 +42,8 @@ func main() {
 	// create web server
 	router := gin.Default()
 
-	// if we get ping request run function c
-	router.GET("/users", func(c *gin.Context) {
-		// send json reponse
+	// if we get ping request and key is valid run function c
+	router.GET("/users", apiKeyAuth(), func(c *gin.Context) {
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
