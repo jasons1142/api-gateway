ef71e5567f2ee31152074ac3f30d32184e71ac2c
Jason Senecharles <jasons11@bu.edu>
2026-06-22 23:56:53 -0400

    Hard-Coded rate limiting

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type RateLimitInfo struct {
+	Count       int
+	WindowStart time.Time
+}
+
+var rateLimits = map[string]RateLimitInfo{}
 
 // authenticating api key
 func apiKeyAuth() gin.HandlerFunc {
@@ -32,6 +40,40 @@ func apiKeyAuth() gin.HandlerFunc {
 	}
 }
 
+// limiting large number of requests
+func rateLimiter() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		apiKey := c.GetHeader("x-api-key")
+
+		limit := 5
+		window := time.Minute
+		now := time.Now()
+
+		info, exists := rateLimits[apiKey]
+
+		if !exists || now.Sub(info.WindowStart) > window {
+			info = RateLimitInfo{
+				Count:       1,
+				WindowStart: now,
+			}
+		} else {
+			info.Count++
+		}
+
+		rateLimits[apiKey] = info
+
+		if info.Count > limit {
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"error": "Too Many Requests",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	// target backend service
 	backendURL, _ := url.Parse("http://localhost:8081")
@@ -43,7 +85,7 @@ func main() {
 	router := gin.Default()
 
 	// if we get ping request and key is valid run function c
-	router.GET("/users", apiKeyAuth(), func(c *gin.Context) {
+	router.GET("/users", apiKeyAuth(), rateLimiter(), func(c *gin.Context) {
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
